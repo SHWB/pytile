2b51e6895bac0cee844c0abef634f6545a6569f6
Caligin@Theleth <caligin35@yahoo.it>
2014-11-12 01:03:19 +0100

    enh: tentative real test

--- src/detect_clusters.py
+++ src/detect_clusters.py
@@ -1,3 +1,26 @@
+import numpy as np
+import cv2
 
 def count(image):
-    return 0
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hue_channel = hsv[:,:,0]
+    sat_channel = hsv[:,:,1]
+    #%%
+    """
+    thresholding seems more effective if done on blurred images
+    the candidate zones are characterized by low Hue and very low Saturation
+    """
+    blurred_sat_channel = cv2.GaussianBlur(sat_channel,(5,5),0)
+    blurred_hue_channel = cv2.GaussianBlur(hue_channel,(5,5),0)
+    #H channel is in range [0,175]
+    reth, hue_mask = cv2.threshold(blurred_hue_channel,50,175,cv2.THRESH_BINARY_INV)
+    rets, sat_mask = cv2.threshold(blurred_sat_channel,60,255,cv2.THRESH_BINARY_INV)
+    mask = cv2.bitwise_and(hue_mask,sat_mask)
+    #%%
+    #apply closing in order to remove 'holes' in our tiles
+    kernel = np.ones((15,15),np.uint8)
+    closed_enough = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    #%%
+    #find the countours
+    contours, hier = cv2.findContours(closed_enough,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    return len(contours)
--- test/detect_clusters_test.py
+++ test/detect_clusters_test.py
@@ -2,12 +2,22 @@ import sys
 sys.path.insert(0, 'src')
 import unittest
 import detect_clusters
+import cv2
+import matplotlib.pyplot as plt
 
 class TestSequenceFunctions(unittest.TestCase):
 
+    def open_sample(self, index):
+        return cv2.imread("test/test_data/test_" + index + ".jpg")
+
+    def test_open_sample(self):
+        image = self.open_sample("001")
+        self.assertNotEqual(image,None)
+
     def test_whatever(self):
-        clusters_count = detect_clusters.count( None )
-        self.assertEqual(0, clusters_count)
+        image = self.open_sample("001")
+        clusters_count = detect_clusters.count( image )
+        self.assertEqual(6, clusters_count)
 
 if __name__ == '__main__':
     unittest.main()
