enh: added straighten to fix tilts in melds; enh: added recognition of the winning tile and related tests; enh: added rotation for the winning tile

src/detect_clusters.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from math import sin, pi
 
 def count(image):
-    contours = fetch_contours(image,100)
+    contours,l = fetch_contours(image,100)
     return len(contours)
 
 def fetch_contours(image, min_length):
@@ -26,43 +26,56 @@ def fetch_contours(image, min_length):
     This explains the condition in the returned list:
     at the resolution we're working at a tile contour should be certainly larger than 100 pixels.
     """
-    return np.array([contour for contour in contours if cv2.arcLength(contour,True) > min_length])
+    candidate_contours = np.array([contour for contour in contours if cv2.arcLength(contour,True) > min_length])
+    candidate_contours_lengths = np.array([cv2.arcLength(candidate, True) for candidate in candidate_contours])
+    return candidate_contours, candidate_contours_lengths
 
-def rotate(image,angle,tolerance=5):
-    """
-    At the moment we obtain melds which are not orthogonal.
-    Need to rotate them if too crooked
-    """
-    if is_somewhat_straight(angle,tolerance):
-        return image
-    else:
-        rows,cols = image.shape
-        #cv2.getRotationMatrix2D(center,angle,scale)
-        M = cv2.getRotationMatrix2D((cols/2,rows/2),-angle,1)
-        dst = cv2.warpAffine(image,M,(cols,rows))
-        return dst
+def get_winning_tile(contours,lengths):
+    winning_tile_index = np.argmin(lengths)
+    return contours[winning_tile_index]
 
 def is_somewhat_straight(angle,tolerance):
     # all quantities in degrees
     return abs(sin(pi/90*angle)) <= sin(pi/90*tolerance)
 
+def straighten(image,angle,tolerance=4):
+    """
+    Rotate melds that are not orthogonal within the tolerance(degrees)
+    """
+    if is_somewhat_straight(angle,tolerance):
+        return image
+    else:
+        rows,cols,channel = image.shape
+        #cv2.getRotationMatrix2D(center,angle,scale), the angle is CCW
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
+        rotated = cv2.warpAffine(image,M,(cols,rows)) 
+        return rotated
 
 if __name__ == '__main__':
-    for i in range(1,8):
-        image = cv2.imread('test/test_data/test_00{}.jpg'.format(i))
-        contours = fetch_contours(image,100) #min_length should be tuned on image size
+    for sample_index in range(1,8):
+        image = cv2.imread('test/test_data/test_00{}.jpg'.format(sample_index))
+        contours, lengths = fetch_contours(image,100) #min_length should be tuned on image size
         if contours.size == 0:
             break
-        j = 1
+        segment_index = 1
         test_data_path = 'test/test_data/'
-        melds_dir = 'melds_00{}'.format(i)
+        melds_dir = 'melds_00{}'.format(sample_index)
+        winning_tile_index = np.argmin(lengths)
         if not os.path.exists(test_data_path + melds_dir):
             os.makedirs(test_data_path + melds_dir)
-        for cnt in contours:
-            (x,y,w,h) = cv2.boundingRect(cnt)
-            rect = cv2.minAreaRect(cnt)
-            #rect: Box2D structure - ( top-left corner(x,y), (width, height), clockwise angle of rotation )
-            meld = image[y:y+h,x:x+w]
-            segment_out = rotate(meld,rect)
-            cv2.imwrite(test_data_path + melds_dir + '/segment_{}.jpg'.format(j), segment_out)
-            j += 1
+        for contour_index in range(len(contours)):
+            (x,y,w,h) = cv2.boundingRect(contours[contour_index])
+            # rotated_box = cv2.fitEllipse(contours[contour_index]) # <-- this doesn't work very well
+            rotated_box = cv2.minAreaRect(contours[contour_index])
+            #rotated_box: Box2D structure - ( top-left corner(x,y), (width, height), clockwise angle of rotation )
+            bounded_meld = image[y:y+h,x:x+w]
+            if contour_index != winning_tile_index:
+                segment_out = straighten(bounded_meld,rotated_box[2])
+                cv2.imwrite(test_data_path + melds_dir + '/segment_{}.jpg'.format(segment_index), segment_out)
+            else:
+                rows,cols,channel = bounded_meld.shape
+                CCW_rotation = cv2.getRotationMatrix2D((cols/2,rows/2),-90,1)
+                segment_out = straighten(bounded_meld,rotated_box[2])
+                segment_out = cv2.warpAffine(segment_out,CCW_rotation,(cols,rows))               
+                cv2.imwrite(test_data_path + melds_dir + '/single_segment_{}.jpg'.format(segment_index), segment_out)         
+            segment_index += 1

test/detect_clusters_test.py
@@ -21,7 +21,7 @@ class TestDetection(unittest.TestCase):
          file_name = str(location_dict.keys()[0])
          internal_points = np.array(location_dict.values()[0])
          image = cv2.imread("test/test_data/" + file_name)
-         contours = detect_clusters.fetch_contours(image,100)
+         contours, l = detect_clusters.fetch_contours(image,100)
          got = [] #got[i] is true if any point in internal_points is in contours[i]
          for cnt in contours:
             is_in_contour = [] #element i is 1 (one) if cnt contains any point in internal_points
@@ -29,6 +29,16 @@ class TestDetection(unittest.TestCase):
               is_in_contour.append(cv2.pointPolygonTest(cnt, tuple(point),False))
             got.append(1 in is_in_contour)
          self.assertNotIn(False,got)
+
+    @file_data('test_data/winner_locations.json')  
+    def test_winner_locations(self,location_dict):
+        file_name = str(location_dict.keys()[0])
+        win_center = np.array(location_dict.values()[0])
+        image = cv2.imread('test/test_data/'+file_name)
+        contours, l = detect_clusters.fetch_contours(image,100)
+        win_tile_contour = detect_clusters.get_winning_tile(contours,l)
+        got = (1 == cv2.pointPolygonTest(win_tile_contour,tuple(win_center),False))
+        self.assertTrue(got)
 
     @data((3,True),(0,True),(-4,True),(140,False),(271,True),(-45,False))
     def test_is_somewhat_straight(self,datapoint):